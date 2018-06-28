f81c1593f994779d6feecb36d899fcc385f3ecba
hannah <hannah@tox.im>
2018-06-28 13:59:53 -0400

    Update to noVNC 1.0.0

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 /**
  * Dependencies.
  */
-var Util = require('./lib/util');
-var Keys = require('./lib/keys');
-var KbdUtil = require('./lib/kbdutil');
-var Input = require('./lib/input');
-var Websock = require('./lib/websock');
+
+var DOMKeyTable = require('./lib/input/domkeytable');
+var FixedKeys = require('./lib/input/fixedkeys');
+var Keyboard = require('./lib/input/keyboard');
+var Keysym = require('./lib/input/keysym');
+var KeysymDef = require('./lib/input/keysymdef');
+var Mouse = require('./lib/input/mouse');
+var Util = require('./lib/input/util');
+var VKeys = require('./lib/input/vkeys');
+var XTScanCodes = require('./lib/input/xtscancodes');
+var Browser = require('./lib/util/browser');
+var Events = require('./lib/util/events');
+var EventTarget = require('./lib/util/eventtarget');
+var Logging = require('./lib/util/logging');
+var Polyfill = require('./lib/util/polyfill');
+var Strings = require('./lib/util/strings');
 var Base64 = require('./lib/base64');
 var DES = require('./lib/des');
-var TINF = require('./lib/tinf');
 var Display = require('./lib/display');
+var Encodings = require('./lib/encodings')
+var Inflator = require('./lib/inflator');
 var RFB = require('./lib/rfb');
+var Websock = require('./lib/websock');
 
 
 
 var noVNC = {
+	DOMKeyTable: DOMKeyTable,
+	FixedKeys: FixedKeys,
+	Keyboard: Keyboard,
+	Keysym: Keysym,
+	KeysymDef: KeysymDef,
+	Mouse: Mouse,
 	Util: Util,
-	Keys: Keys,
-	KbdUtil: KbdUtil,
-	Input: Input,
-	Websock: Websock,
+	VKeys: VKeys,
+	XTScanCodes: XTScanCodes,
+	Browser: Browser,
+	Events: Events,
+	EventTarget: EventTarget,
+	Logging: Logging,
+	Polyfill: Polyfill,
+	Strings: Strings,
 	Base64: Base64,
 	DES: DES,
-	TINF: TINF,
 	Display: Display,
-	RFB: RFB
+	Encodings: Encodings,
+	Inflator: Inflator,
+	RFB: RFB,
+	Websock: Websock
 };
 
 
